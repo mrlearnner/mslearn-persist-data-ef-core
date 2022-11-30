53fb83bf58000cd913f90222dadd351992aff0d9
mrlearnner <mr.learn.ner105@gmail.com>
2022-11-30 11:13:28 -0500

    success using ReverseEngineering

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSwaggerGen();
 // Add the PizzaContext
 builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
 // Add the PromotionsContext
+builder.Services.AddSqlite<PromotionsContext>("Data Source=./Promotions/Promotions.db");
 
 builder.Services.AddScoped<PizzaService>();
 
